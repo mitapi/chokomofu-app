78a07c1e08f15c3ea55db34ac50efa78b7031c05
mitapi <popoyo495@gmail.com>
2025-09-30 00:27:36 +0900

    Precompile assets on boot; enable static file serving in production

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,3 +7,13 @@ rm -f tmp/pids/server.pid
 
 # （必要になったら前処理をここに足せる）
 exec "$@"
+
+# DB migrate & seed
+bundle exec rails db:migrate
+bundle exec rails db:seed
+
+# ここで本番用アセットをビルド
+bundle exec rails assets:precompile
+
+# Puma 起動
+exec bundle exec puma -C config/puma.rb
