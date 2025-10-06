07fc58851f2ee24058520901b5f001414287834b
mitapi <popoyo495@gmail.com>
2025-10-07 00:13:30 +0900

    匿名ユーザー機能 model・configまでOK

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  validates :guest_uid, uniqueness: true, presence: true
+  validates :nickname, presence: true, length: { maximum: 10 }
+
+  has_many :user_characters
+  has_many :interactions
+  has_many :inquiries
+
+  enum auth_kind: {
+    guest:     0,
+    password:  1,
+  }, _default: :guest 
 end
