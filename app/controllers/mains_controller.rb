c473bfff287cab1705e30d7da2e84de60b99269f
mitapi <popoyo495@gmail.com>
2025-10-12 01:22:32 +0900

    匿名ユーザー手動テスト途中・初回画面作ってから続きを行う

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,3 +1,12 @@
 class MainsController < ApplicationController
+  before_action :require_onboarding
+
   def show; end
+
+  private
+
+  def require_onboarding
+    return if @current_user.nickname.present?
+    redirect_to nickname_path
+  end  
 end
