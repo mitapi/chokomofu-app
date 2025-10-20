fd942df5ebcdee97279b83d7d00dc212beb69bdd
mitapi <popoyo495@gmail.com>
2025-10-21 01:44:02 +0900

    Rspec書き終えた、次テストしてみる

--- spec/requests/guest_user_spec.rb
+++ spec/requests/guest_user_spec.rb
@@ -2,5 +2,76 @@ require 'rails_helper'
 
 RSpec.describe "Anonymous Guest Cookie (guest_uid)", type: :request do
   include ActiveSupport::Testing::TimeHelpers
+  include RequestCookieHelpers
+  let(:path) { main_path }
 
+  describe "初回訪問（Cookieなし）" do
+    it "新規発行(Set-Cookie)" do
+      # Userを1行作成、Set-Cookie に guest_uid=... を含む
+      uid = issue_first_cookie
+
+      # Set-Cookie に HttpOnly / SameSite=Lax を含んでいる" do
+      expect_cookie_attributes(response.headers["Set-Cookie"])
+    end
+  end
+
+  describe "再訪（正常）" do
+    it "同日アクセスで User.count は増えない、uid再発行はしない" do
+      uid = issue_first_cookie
+
+      # 同日アクセスで User.count は増えない
+      set_next_request_cookie("guest_uid", uid)
+      expect{ get path, headers: next_request_headers}.not_to change{ User.count }
+
+      # 原則、同日中は再発行しない（= Set-Cookie に guest_uid を含まない）
+      expect(contains_cookie_name?("guest_uid")).to be(false)
+      # ↓は guest_uid は含まれていないけど、DBにはユーザーがちゃんと存在しているかをチェックしています
+      expect(User.find_by(uid: uid)).to be_present
+    end
+  end
+
+  describe "Cookie破損（署名不一致 / 形式不正）" do
+    it "安全に再発行し、必要ならUserを再作成/再取得する" do
+      @next_cookie_header = "guest_uid=this-is-not-a-uuid"
+      before_count = User.count
+
+      # 不正なuidを受け取ってもエラーで落ちない
+      get path, headers: next_request_headers
+      expect(response).to have_http_status(:ok)
+
+      # 新しいuidを再発行する
+      expect(contains_cookie_name?("guest_uid")).to be(true)
+      new_uid = extract_cookie_value("guest_uid")
+      expect(new_uid).to be_present
+      expect(User.find_by(guest_uid: new_uid)).to be_present
+
+      # 二重発行していないか？
+      set_cookie = response.headers["Set-Cookie"]
+      expect(set_cookie.scan("guest_uid=").size).to eq(1)
+
+      # データがあって再取得の場合は増えないし、データがなければ新規作成される（ので、>=にしています）
+      expect(User.count).to be >= before_count
+    end
+  end
+
+  describe "D) DB欠損（Cookie正常だが行が消えた）" do
+    it "同じUIDでUserを再作成して復旧する" do
+      uid = issue_first_cookie
+      # DB欠損を手動で作る
+      User.where(guest_uid: uid).delete_all
+      expect(User.find_by(guest_uid: new_uid)).to be_nil
+
+      # 同じCookieで再訪
+      set_next_request_cookie("guest_uid", uid)
+      expect{ get path, headers: next_request_headers}.to change{ User.count }.by(1) 
+      expect(response).to have_http_status(:ok)
+      expect(user_by(uid)).to be_present
+
+      # 日次更新（Cookie再発行）を同時にテストしないように
+      expect(contains_cookie_name?("guest_uid")).to be(false)
+    end
+  end
+
+  # describe "E) 日次更新（JST 0:00 跨ぎ）" do
+    # it "当日中は未更新・翌日になったらSet-Cookieで更新する" do
 end
--- spec/support/request_cookie_helpers.rb
+++ spec/support/request_cookie_helpers.rb
@@ -1,5 +1,54 @@
 module RequestCookieHelpers
+
+  # set-cookieの値を取り出す
   def extract_cookie_value(name)
-    
+    set_cookie = responce.headers["Set-Cookie"]
+    return nil unless set_cookie
+    m = set_cookie.match(/#{Regexp.escape(name)=([^;]+)}/)
+    m && m[1]
+    # m は配列ではなく、Rubyの MatchData オブジェクト。m[1]は、([^;]+)の部分。
+  end
+
+  # Set-Cookie に guest_uid=...という記述が含まれる
+  def contains_cookie_name?(name)
+    set_cookie = response.headers["Set-Cookie"]
+    !!(set_cookie && set_cookie.include?("#{name}="))
+  end
+
+  # 初回cookie発行。Userを1行作成、Set-Cookie に guest_uid=... を含む
+  def issue_first_cookie
+    expect{ get main_path }.to change{ User.count }.by(1) 
+    expect(response).to have_http_status(:ok) 
+
+    expect(contains_cookie_name?("guest_uid")).to be(true)
+    uid = extract_cookie_value("guest_uid")
+
+    # guest_uidが空ではないか
+    expect(uid).to be_present
+
+    # guest_uidに対応するユーザーは存在しているか
+    expect(User.find_by(guest_uid: uid)).to be_present
+    return uid
+  end
+
+  # Set-Cookie に HttpOnly / SameSite=Lax を含んでいるか
+  # Secure は本番のみ付与方針なので、テストでは必須にしない
+  def expect_cookie_attributes(set_cookie_string)
+    expect(set_cookie_string).to include("HttpOnly")
+    expect(set_cookie_string).to include("SameSite=Lax")
+  end
+
+  # 次に送るHTTPリクエストに Cookie を含める準備をする
+  def set_next_request_cookie(name, value)
+    @next_cookie_header = "#{name}=#{value}"
+  end
+
+  # 次のHTTPリクエストで送信するヘッダーを準備する
+  def next_request_headers
+    if @next_cookie_header
+      { "Cookie" => @next_cookie_header }
+    else
+      {}
+    end
   end
 end
