27869631933f8f670ba06206dfe37d122be37e76
mitapi <popoyo495@gmail.com>
2025-10-23 22:39:07 +0900

    nickname_controller作成

--- app/controllers/nicknames_controller.rb
+++ app/controllers/nicknames_controller.rb
@@ -1,4 +1,22 @@
 class NicknamesController < ApplicationController
-  def show; end
-  def create; end
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(nickname_params)
+      flash[:notice] = "ニックネームを保存しました"
+      redirect_to main_path
+    else
+      flash.now[:danger] = "ニックネームを保存できませんでした"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname)
+  end
 end
