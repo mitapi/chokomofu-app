61a0e0e757413eed0dd4adcaaa770d1656519d86
mitapi <popoyo495@gmail.com>
2025-10-24 01:22:21 +0900

    nickname view 作成中

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,14 @@ class User < ApplicationRecord
 
   validates :guest_uid, uniqueness: true, presence: true
   validates :nickname,
-    presence: { message: "ニックネームを入力してください" },
     length: { maximum: 10, message: "ニックネームは10文字以内で入力してください" },
     format:   {
       # ひらがな・カタカナ・漢字・英数字・スペース（全/半角）のみ許可。絵文字や記号は不許可
       with: /\A[[:alnum:]\p{Hiragana}\p{Katakana}\p{Han}\p{Zs}]+\z/u,
       message: "使用できない文字が含まれています"
     },
-    allow_nil: false
-    unless: :guest?
+    allow_nil: true,
+    allow_blank: true
 
   has_many :user_characters
   has_many :interactions
@@ -28,5 +27,4 @@ class User < ApplicationRecord
     return if nickname.nil?
     self.nickname = nickname.gsub(/\A([[:space:]\u3000])+|([[:space:]\u3000])+\z/u, "")
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'top#index'
   resource :main, only: %i[show] 
   resource :chat, only: %i[show] 
-  resource :nickname, only: %i[edit update] 
+  resource :nickname, only: [:edit, :update]
+
+  get "/nickname", to: "nicknames#edit"
   get "up" => "rails/health#show", as: :rails_health_check
 end
