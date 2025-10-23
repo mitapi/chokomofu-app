697c5b59b97acca406e7ea99849bec17bbfdc7af
mitapi <popoyo495@gmail.com>
2025-10-23 22:39:07 +0900

    nickname_controller作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
+  before_validation :normalize_nickname
+
   validates :guest_uid, uniqueness: true, presence: true
-  # validates :nickname,
-            # presence: true,
-            # length: { maximum: 30 },
-            # unless: :guest?
+  validates :nickname,
+    presence: { message: "ニックネームを入力してください" },
+    length: { maximum: 10, message: "ニックネームは10文字以内で入力してください" },
+    format:   {
+      # ひらがな・カタカナ・漢字・英数字・スペース（全/半角）のみ許可。絵文字や記号は不許可
+      with: /\A[[:alnum:]\p{Hiragana}\p{Katakana}\p{Han}\p{Zs}]+\z/u,
+      message: "使用できない文字が含まれています"
+    },
+    allow_nil: false
+    unless: :guest?
 
   has_many :user_characters
   has_many :interactions
@@ -13,4 +21,12 @@ class User < ApplicationRecord
     guest:     0,
     password:  1,
   }, _default: :guest 
+
+  private
+
+  def normalize_nickname
+    return if nickname.nil?
+    self.nickname = nickname.gsub(/\A([[:space:]\u3000])+|([[:space:]\u3000])+\z/u, "")
+  end
+
 end
