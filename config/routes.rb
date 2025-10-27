1f3970b586b82ea0a2f4d1b839a5e8c6b6449541
mitapi <popoyo495@gmail.com>
2025-10-28 00:27:54 +0900

    nickname view作成中

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resource :main, only: %i[show] 
   resource :chat, only: %i[show] 
   resource :nickname, only: [:edit, :update]
+  resource :account, only: [:edit, :update]
 
   get "/nickname", to: "nicknames#edit"
+  get "/account", to: "nicknames#edit"
   get "up" => "rails/health#show", as: :rails_health_check
 end
