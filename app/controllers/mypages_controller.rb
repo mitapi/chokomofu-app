dede3a44d02a7903845cd614b2f152b9c40c0cca
mitapi <popoyo495@gmail.com>
2026-01-07 22:46:35 +0900

    userモデル、コントローラー編集（バリデーションなど）

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -3,11 +3,26 @@ class MypagesController < ApplicationController
   end
 
   def edit_nickname
+    @user = current_user
   end
 
   def update_nickname
+    @user = current_user
+
+    if @user.save
+      flash[:notice] = "プロフィールを更新しました"
+      redirect_to mypage_nickname_path
+    else
+      render :edit_nickname, status: :unprocessable_entity
+    end
   end
 
   def how_to_play
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :region)
   end
 end
