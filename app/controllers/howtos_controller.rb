23e168bf2b50e0bd1e1fce78c317ca5ffa9fca27
mitapi <popoyo495@gmail.com>
2026-02-25 23:09:08 +0900

    howtoページview編集中

--- app/controllers/howtos_controller.rb
+++ app/controllers/howtos_controller.rb
@@ -1,4 +1,11 @@
-class HowtosController < ApplicationController
+class HowtoController < ApplicationController
   def show
+    @from = params[:from]
+    @back_path =
+      case @from
+      when "mypage" then how_to_play_mypage_path
+      else
+        main_path
+      end
   end
 end
