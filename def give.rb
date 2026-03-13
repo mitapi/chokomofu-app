bb687f13e2ea08d3efbdb9dc15fd66b864b35c98
mitapi <popoyo495@gmail.com>
2026-03-14 01:21:50 +0900

    app/views/welcomes/guide.html.erb修正

--- def give.rb
+++ def give.rb
@@ -20,3 +20,14 @@
         locals: { character: character, snack_type: snack_key, lines: lines, limited: true }
       )
     end
+
+
+                  <%= link_to howto_path(from: "welcome"),
+                aria:  { label: "遊び方説明（howto)ページに遷移" },
+                class: "group
+                        font-bold flex h-10 items-center justify-center z-30
+                        rounded-md inline-block bg-amber-400 px-4 py-2 text-white hover:bg-orange-400" do %>
+                <span class="block group-active:[transform:translate3d(0,1px,0)]">
+                  遊び方ガイドを見てみる！
+                </span>
+              <% end %>
